61cba2d1fceb21d65be74220fb7a6286f9928f6d
GregoryGalicio <gregory.galicios@ciplima.org.pe>
2022-07-06 05:31:48 -0500

    form, crud y cards conectados con useEffect

--- src/components/crudApp/CrudApp.jsx
+++ src/components/crudApp/CrudApp.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import CrudForm from '../crudForm/CrudForm'
 
 const CrudApp = () => {
   return (
     <div>
         <h1>Servicio</h1>
+        <CrudForm/>
 
     </div>
   )
--- src/components/crudForm/CrudForm.jsx
+++ src/components/crudForm/CrudForm.jsx
@@ -1,15 +1,21 @@
 
 import React, { useState,useEffect } from 'react';
+import { serviceData } from '../../serviceDataBase';
 
-const initialForm={
-    name:"",
-    description:"",
-    id: null,
-};
+// const initialForm={
+//     name:"",
+//     description:"",
+//     id: null,
+// };
 
 
 const CrudForm = () => {
-    const [form, setForm] = useState(initialForm);
+    const [form, setForm] = useState([]);
+
+    useEffect(() => {
+        setForm(serviceData);
+   }, [])
+
 
     const handleChange = (e) => {}
     const handleSubmit = (e) => {}
@@ -19,10 +25,13 @@ const CrudForm = () => {
 
   return (
     <div>
-        <h3>Agregar</h3>
+        <h2>Agregar Servicio</h2>
         <form onSubmit={handleSubmit}>
+            <h3>Nombre</h3>
             <input type="text" name='name' placeholder='Nombre' onChange={handleChange} value={form.name}/>
+            <h3>Servicio</h3>
             <input type="text" name='description' placeholder='Descripción' onChange={handleChange} value={form.description}/>
+            <br/>
             <input type="submit" value="Grabar"/>
             <input type="submit" value="Cancelar" onClick={handleReset}/>
         </form>
