dd8b6247c1aa44744a8eabdeaf01a6d5ec2aef35
GregoryGalicio <gregory.galicios@ciplima.org.pe>
2022-07-05 22:12:29 -0500

    creando components header y filter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
-import Footer from './componets/Footer/Footer'
+import Footer from './componets/footer/Footer'
 
 function App() {
   const[darkMode,setDarkMode]=useState(false);
@@ -13,19 +13,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Gregory
-        </a>
-        <Footer darkMode={darkMode}/>
       </header>
+      <Footer darkMode={darkMode}/>
     </div>
   );
 }
