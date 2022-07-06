9e58f4b3433a9a99d7d8d33d1c865c1c796a95ef
GregoryGalicio <gregory.galicios@ciplima.org.pe>
2022-07-06 14:24:51 -0500

    primer deploy

--- src/components/crudApp/CrudApp.jsx
+++ src/components/crudApp/CrudApp.jsx
@@ -1,4 +1,4 @@
-import React, {useState,useEffect} from 'react'
+import React, {useState} from 'react'
 import Cards from '../cards/Cards';
 import CrudForm from '../crudForm/CrudForm'
 import "./CrudApp.css";
